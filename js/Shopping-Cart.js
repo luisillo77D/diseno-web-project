6b96f6f4a787501f7327de658a6ad84d1cdac2cc
luisillo77D <luisillo77mario@gmail.com>
2024-05-19 21:18:24 -0700

    chore: Refactor Shopping Cart code for improved readability and performance

--- js/Shopping-Cart.js
+++ js/Shopping-Cart.js
@@ -181,18 +181,18 @@ function actualizarTotalCarrito() {
 
     for (var i = 0; i < carritoItems.length; i++) {
         var item = carritoItems[i];
-        var precioElemento = item.getElementsByClassName('carrito-item-precio')[0];
-        //quitamos el simobolo peso y el punto de milesimos.
-        var precio = parseFloat(precioElemento.innerText.replace('$','').replace('.',''));
-        var cantidadItem = item.getElementsByClassName('carrito-item-cantidad')[0];
-        console.log(precio);
-        var cantidad = cantidadItem.value;
-        total = total + (precio * cantidad);
+        var precioElemento = item.querySelector('.carrito-item-precio');
+        var precio = parseFloat(precioElemento.innerText.replace(/[^\d.]/g, '')); // Eliminar símbolos no numéricos
+        var cantidadItem = item.querySelector('.carrito-item-cantidad');
+        var cantidad = parseInt(cantidadItem.value);
+
+        if (!isNaN(precio) && !isNaN(cantidad)) {
+            total += precio * cantidad;
+        }
     }
-    total = Math.round(total * 100)/100;
 
-    document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
-
+    var totalCarritoElemento = document.getElementById('total-carrito');
+    totalCarritoElemento.innerText = '$' + total.toLocaleString("es", { minimumFractionDigits: 2 }) + ",00";
 }
 
 // ------------------- CODIGO QR ---------------------
